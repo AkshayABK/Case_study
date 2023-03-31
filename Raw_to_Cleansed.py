1f71f903a521c763f5326e0a64d742bd159fed36
Akshay Kore <92500657+AkshayABK@users.noreply.github.com>
2023-03-31 21:42:28 +0530

    Update Raw_to_Cleansed.py

--- Raw_to_Cleansed.py
+++ Raw_to_Cleansed.py
@@ -8,7 +8,7 @@ def add_column_and_transfer_file(event, context):
 
     file_name = event['name']
     client = storage.Client()
-    bucket = client.get_bucket('akshay_raw_bucket')
+    bucket = client.get_bucket('source_raw_bucket')
     blob = bucket.blob(file_name)
     content = blob.download_as_string()
 
@@ -25,7 +25,7 @@ def add_column_and_transfer_file(event, context):
     df['region']=df['region'].fillna(df.region.mode()[0])
 
     new_file_name = "Cleansed_"+file_name
-    destination_bucket=client.get_bucket("akshay_cleansed_bucket")
+    destination_bucket=client.get_bucket("destination_curated_bucket")
     new_blob = destination_bucket.blob(new_file_name)
     new_blob.upload_from_string(df.to_csv(index=False), 'text/csv')
 
